97be97903040379a1180cfa1529f4da0ae9b2f3b
Imran Khan <imrank.developer@gmail.com>
2020-07-03 12:05:18 +0530

    Initial Changes

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+    apiKey: "AIzaSyCoA_61RtzdQ0ogLT7O3KE8f4YsIQLyKhk",
+    authDomain: "mygithub-21ae6.firebaseapp.com",
+    databaseURL: "https://mygithub-21ae6.firebaseio.com",
+    projectId: "mygithub-21ae6",
+    storageBucket: "mygithub-21ae6.appspot.com",
+    messagingSenderId: "752903042443",
+    appId: "1:752903042443:web:0613fdf11dce1878842b8e",
+    measurementId: "G-6GH5XR6SPM"
+  };
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
